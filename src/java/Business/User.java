b150f285c199029958bebc924c3afa96998192a4
david <david@david-netbook>
2014-11-20 17:08:56 +0000

    comments

--- src/java/Business/User.java
+++ src/java/Business/User.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Business;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,15 +8,21 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class User {
 
+    /***
+     * Business functionality for logging in 
+     * @param request
+     * @return request with a set session and the attribute "include" set to the correct page
+     */
     public static HttpServletRequest Login(HttpServletRequest request) {
-
+        DatabaseModel.User user  = new DatabaseModel.User();
+        
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        DatabaseModel.User user = new DatabaseModel.User();
-
-        user.findUser(username, password);
-
+        if(validLoginParameters(username, password)){
+            user.findUser(username, password);
+        }
+        
         if (user.getID() > 0) {
             request.getSession().setAttribute("user", user);
             request.setAttribute("include", "homepage.jsp");
@@ -33,6 +34,44 @@ public class User {
         return request;
     }
 
+    /***
+     * Checks to see if the login parameters length > 0
+     * @param username
+     * @param password
+     * @return true both username and password length > 0
+     *         false one failed
+     */
+    private static boolean validLoginParameters(String username, String password){
+        boolean isValid = false;
+        
+        if(validateStringLength(username) & validateStringLength(password)){
+            isValid = true; 
+        }
+        
+        return isValid;
+    }
+    
+    /***
+     * Checks the string length > 0
+     * @param param
+     * @return  true string length > 0
+     *          false string length == 0 
+     */
+    private static boolean validateStringLength(String param){
+        boolean isValid = false;
+        
+        if(param.length() > 0){
+            isValid = true;
+        }
+        
+        return isValid;
+    }
+    
+    /***
+     * Destroys session and sets request attribute "include" to the "login.jsp"
+     * @param request
+     * @return 
+     */
     public static HttpServletRequest Logout(HttpServletRequest request) {
 
         request.getSession().invalidate();
