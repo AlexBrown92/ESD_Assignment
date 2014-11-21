82f0f59c4f8da26eec7ffb56ae4fa36f7ff5c383
Alex <Alex@Alex-ThinkPad>
2014-11-21 19:45:28 +0000

    Adding Models

---
+++ src/java/DatabaseModel/Bill.java
@@ -0,0 +1,19 @@
+
+package DatabaseModel;
+
+import Utils.DBA;
+import Utils.Helper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Alex
+ */
+public class Bill {
+    int id;
+    int patientId;
+    
+    
+}
