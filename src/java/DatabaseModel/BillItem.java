5259654441e68bbaff6ce8a4142c98d2db7b6fe9
Alex Brown <aj.brown92@gmail.com>
2014-11-25 11:50:24 +0000

    Adding Bill/BillItem Models

--- src/java/DatabaseModel/BillItem.java
+++ src/java/DatabaseModel/BillItem.java
@@ -5,10 +5,75 @@
  */
 package DatabaseModel;
 
+import Utils.DBA;
+import Utils.Helper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author Alex
  */
 public class BillItem {
-    
+
+    int billId;
+    int medicineId;
+    int quanitity;
+
+    public BillItem() {
+        this.billId = 0;
+        this.medicineId = 0;
+        this.quanitity = 0;
+    }
+
+    public int getBillId() {
+        return billId;
+    }
+
+    public void setBillId(int billId) {
+        this.billId = billId;
+    }
+
+    public int getMedicineId() {
+        return medicineId;
+    }
+
+    public void setMedicineId(int medicineId) {
+        this.medicineId = medicineId;
+    }
+
+    public int getQuanitity() {
+        return quanitity;
+    }
+
+    public void setQuanitity(int quanitity) {
+        this.quanitity = quanitity;
+    }
+
+    public BillItem findBillItem(int billId, int medicineId) {
+        String query = "SELECT `billItem`.`billId`, "
+                + "         `billItem`.`medicineId`, "
+                + "         `billItem`.`quantity` "
+                + "     FROM `billItem` "
+                + "     WHERE `billItem`.`billId` = '%d'"
+                + "     AND `billItem`.`medicineId` = '%d';";
+        DBA dba = Helper.getDBA();
+        try {
+            ResultSet rs = dba.executeQuery(String.format(query, billId, medicineId));
+
+            while (rs.next()) {
+                this.billId = rs.getInt("billId");
+                this.medicineId = rs.getInt("medicineId");
+                this.quanitity = rs.getInt("quantity");
+            }
+            rs.close();
+            dba.closeConnections();
+
+        } catch (SQLException sqlEx) {
+            dba.closeConnections();
+            Helper.logException(sqlEx);
+        }
+        return this;
+    }
+
 }
